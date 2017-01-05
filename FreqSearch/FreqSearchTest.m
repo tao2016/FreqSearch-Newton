ee097d595db8d81e5d5e84d08c6009430ac215e7
ZhouBotao <1015849958@qq.com>
2017-01-05 11:22:28 +0800

    测试时加入噪音；模拟AD量化过程；将单一频率改为一组频率值测量，测试OK！

--- FreqSearch/FreqSearchTest.m
+++ FreqSearch/FreqSearchTest.m
@@ -5,23 +5,43 @@ num = 4096;
 win = parzenwin(num);
 t = (1:num)/fs;  t = t(:);
 
-data = 220 * sin(2*pi*51*t) .* win;
-FData = fft(data);
-idx = FindMaxIndex(FData);
-fTemp = (idx-1)*fs/num;
-fLow = fTemp - 1.5;
-fHigh = fTemp + 1.5;
+FL = 8;
+FH = 450;
+FStep = 0.5;
+Fb = FL : FStep : FH;
+FError = Fb * 0;
+FCalc = Fb * 0; %计算值
+ADPresion = 16;
+for k = 1 : length(Fb)
+    data = (sin(2*pi*Fb(k)*t) + 0.01*randn(num, 1));
+    % AD量化，并加窗
+    data = round(data .* 2^ADPresion) .* win; % data = round(data .* 2^ADPresion) .* win/2^ADPresion;
+    FData = fft(data);
+    [~, idx] = max(abs(FData));
+    %idx = FindMaxIndex(abs(FData));
+    fTemp = (idx-1)*fs/num;
+    fLow = fTemp - 1.5;
+    fHigh = fTemp + 1.5;
 
-freq_test = 40 : 0.01 : 60;
-[amp, rx, ix] = ddft(data, fs, freq_test);
+%     freq_test = 40 : 0.01 : 60;
+%     [amp, rx, ix] = ddft(data, fs, freq_test);
 
-%figure('Name','sin-wave');
-% [f1_res, rx1, ix1] = diff1(data, fs, freq_test);
-% [f2_res, rx2, ix2] = diff2(data, fs, freq_test);
-% plot(freq_test, amp.^2, 'r', freq_test, f1_res, 'b', freq_test, f2_res, 'k'); grid on; legend('amp2', 'f1', 'f2'); hold on;
-% [freq, cnt] = NewtonSearch(@diff1, @diff2, 0.0001, 10, 51.5, data, fs);
+    %figure('Name','sin-wave');
+    % [f1_res, rx1, ix1] = diff1(data, fs, freq_test);
+    % [f2_res, rx2, ix2] = diff2(data, fs, freq_test);
+    % plot(freq_test, amp.^2, 'r', freq_test, f1_res, 'b', freq_test, f2_res, 'k'); grid on; legend('amp2', 'f1', 'f2'); hold on;
+    % [freq, cnt] = NewtonSearch(@diff1, @diff2, 0.0001, 10, 51.5, data, fs);
 
-plot(freq_test, amp, 'b'); grid on;  hold on;
-[freq, x1, x2, cnt] = GoldenSection(@ddft, 1, 20, fLow, fHigh, data, fs);
+    %plot(freq_test, amp, 'b'); grid on;  hold on;
+    [freq, x1, x2, cnt] = GoldenSection(@ddft, 0.0001, 20, fLow, fHigh, data, fs);
 
-[frequent, count] = NewtonSearch(@diff1, @diff2, 0.0001, 10, freq, data, fs);
+    %[frequent, count] = NewtonSearch(@diff1, @diff2, 0, 2, freq, data, fs);
+    FCalc(k) = frequent;
+    FError(k) = frequent - Fb(k);
+end
+
+figure('Name', 'FreqError');
+subplot(2,1,1);
+plot(Fb, FError); xlabel('Freq/Hz'); ylabel('FreqError/Hz'); grid on;
+subplot(2,1,2);
+plot(Fb, FCalc); xlabel('FreqReal/Hz'); ylabel('FreqCalc/Hz'); grid on;
--- FreqSearch/GoldenSection.m
+++ FreqSearch/GoldenSection.m
@@ -16,14 +16,14 @@ for k = 1 : n
         b = x2;
         x2 = x1;
         y2 = y1;
-        plot(x2, y2, '*r');
+        %plot(x2, y2, '*r');
         x1 = b - GoldenRatio*(b-a);
         y1 = f(data, fs, x1);
     else
         a = x1;
         x1 = x2;
         y1 = y2;
-        plot(x1, y1, '*r');
+        %plot(x1, y1, '*r');
         x2 = a + GoldenRatio*(b-a);
         y2 = f(data, fs, x2);
     end
@@ -33,4 +33,4 @@ x1 = a;
 x2 = b;
 cnt = k;
 x = (a + b)/2;
-plot(x, f(data, fs, x), '*r');
+%plot(x, f(data, fs, x), '*r');
