bd83fdfd4fcd3c0855752a6e7593248e5ae24532
ZhouBotao <1015849958@qq.com>
2017-01-12 16:48:24 +0800

    测试时增加间谐波；黄金分割法与牛顿法结合使用

--- FreqSearch/FreqSearchTest.m
+++ FreqSearch/FreqSearchTest.m
@@ -5,19 +5,21 @@ num = 4096;
 win = parzenwin(num);
 t = (1:num)/fs;  t = t(:);
 
-FL = 8;
-FH = 450;
-FStep = 0.5;
+FL = 40;
+FH = 80;
+FStep = 0.1;
 Fb = FL : FStep : FH;
 FError = Fb * 0;
 FCalc = Fb * 0; %计算值
 ADPresion = 16;
+
+xInter = 0*sin(2*pi*60*t); %间谐波
 for k = 1 : length(Fb)
-    data = (sin(2*pi*Fb(k)*t) + 0.01*randn(num, 1));
+    data = (sin(2*pi*Fb(k)*t) + xInter + 0.01*randn(num, 1));
     % AD量化，并加窗
     data = round(data .* 2^ADPresion) .* win; % data = round(data .* 2^ADPresion) .* win/2^ADPresion;
     FData = fft(data);
-    [~, idx] = max(abs(FData));
+    [~, idx] = max(abs(FData)); % 使用库函数max，返回最大值及最大值的索引
     %idx = FindMaxIndex(abs(FData));
     fTemp = (idx-1)*fs/num;
     fLow = fTemp - 1.5;
@@ -33,9 +35,9 @@ for k = 1 : length(Fb)
     % [freq, cnt] = NewtonSearch(@diff1, @diff2, 0.0001, 10, 51.5, data, fs);
 
     %plot(freq_test, amp, 'b'); grid on;  hold on;
-    [freq, x1, x2, cnt] = GoldenSection(@ddft, 0.0001, 20, fLow, fHigh, data, fs);
+    [freq, x1, x2, cnt] = GoldenSection(@ddft, 0.5, 20, fLow, fHigh, data, fs);
 
-    %[frequent, count] = NewtonSearch(@diff1, @diff2, 0, 2, freq, data, fs);
+    [frequent, count] = NewtonSearch(@diff1, @diff2, 0.0001, 10, freq, data, fs);
     FCalc(k) = frequent;
     FError(k) = frequent - Fb(k);
 end
